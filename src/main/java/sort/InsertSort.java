fa0ac913c5c1021f763800c6dc4a78cd382325dd
Keid <kssk96@gmail.com>
2017-03-28 00:27:12 +0900

    Add insert sort.

---
+++ src/main/java/sort/InsertSort.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2017 Keid
+*/
+package sort;
+
+import java.util.List;
+
+/**
+ * Insert sort
+ * 
+ * @author Keid
+ */
+public class InsertSort implements Sort<Integer> {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see sort.Sort#sort(java.util.List)
+     */
+    @Override
+    public void sort(List<Integer> list) {
+
+        int num = list.size();
+        for (int sortedNum = 0; sortedNum < num - 1; ++sortedNum) {
+
+            int insertItem = list.get(sortedNum + 1);
+
+            // linear search
+            int insertIndex;
+            for (insertIndex = 0; insertIndex <= sortedNum; ++insertIndex) {
+                if (list.get(insertIndex) > insertItem) {
+                    break;
+                }
+            }
+
+            // insert
+            while (insertIndex <= sortedNum + 1) {
+                int temp = list.get(insertIndex);
+                list.set(insertIndex, insertItem);
+                insertItem = temp;
+                ++insertIndex;
+            }
+
+        }
+
+    }
+
+}
