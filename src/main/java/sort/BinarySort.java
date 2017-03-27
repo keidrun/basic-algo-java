d40bf866f1db69ac7be08f005938fa2663701d05
Keid <kssk96@gmail.com>
2017-03-28 00:39:41 +0900

    Add binary sort.

---
+++ src/main/java/sort/BinarySort.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2017 Keid
+*/
+package sort;
+
+import java.util.List;
+
+/**
+ * Binary sort
+ * 
+ * @author Keid
+ */
+public class BinarySort implements Sort<Integer> {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see sort.Sort#sort(java.util.List)
+     */
+    @Override
+    public void sort(List<Integer> list) {
+
+        int num = list.size();
+        for (int sortedNum = 1; sortedNum < num; ++sortedNum) {
+
+            int insertItem = list.get(sortedNum);
+
+            // binary search
+            int insertIndex;
+            int left = 0, right = sortedNum;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (list.get(mid) < insertItem) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            insertIndex = left;
+
+            // insert
+            while (insertIndex <= sortedNum) {
+                int temp = list.get(insertIndex);
+                list.set(insertIndex, insertItem);
+                insertItem = temp;
+                ++insertIndex;
+            }
+        }
+    }
+
+}
